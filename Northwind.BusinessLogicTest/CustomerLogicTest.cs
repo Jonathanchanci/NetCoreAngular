5fda8b5afad15d425c1e11677143b69d3919f600
Jonathan <jonathan.a.chanci@gmail.com>
2021-05-03 21:14:12 -0500

    Creacion de proyecto para UnitTest

---
+++ Northwind.BusinessLogicTest/CustomerLogicTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NetCoreAngular.BussinessLogic.Implementations;
+using NetCoreAngular.BussinessLogic.Interfaces;
+using NetCoreAngular.UnitOfWork;
+using Northwind.BusinessLogicTest.Mocked;
+using Xunit;
+
+namespace Northwind.BusinessLogicTest
+{
+    public class CustomerLogicTest
+    {
+        private readonly IUnitOfWork _unitMocked;
+        private readonly ICustomerLogic _customerLogic;
+        public CustomerLogicTest()
+        {
+            var unitMocked = new CustomerRepositoryMocked();
+            _unitMocked = unitMocked.GetInstance();
+            _customerLogic = new CustomerLogic(_unitMocked);
+        }
+
+        [Fact]
+        public void GetById_Customer_Test()
+        {
+            var result = _customerLogic.GetById(1);
+
+            result.Should().NotBeNull();
+            result.Id.Should().BeGreaterThan(0);
+        }
+    }
+}
---
+++ Northwind.BusinessLogicTest/Mocked/CustomerRepositoryMocked.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture;
+using Moq;
+using NetCoreAngular.Models;
+using NetCoreAngular.Repositories;
+using NetCoreAngular.UnitOfWork;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Northwind.BusinessLogicTest.Mocked
+{
+    public class CustomerRepositoryMocked
+    {
+        private readonly List<Customer> _customers;
+
+        public CustomerRepositoryMocked() {
+            _customers = Customers();
+        }
+
+        public IUnitOfWork GetInstance()
+        {
+            var mocked = new Mock<IUnitOfWork>();
+            mocked.Setup(u => u.Customer).Returns(GetCustomerRepositoryMocked());
+
+            return mocked.Object;
+        }
+
+        public ICustomerRepository GetCustomerRepositoryMocked()
+        {
+            var customerMocked = new Mock<ICustomerRepository>();
+            customerMocked.Setup(c => c.GetById(It.IsAny<int>()))
+                .Returns((int id) => _customers.FirstOrDefault(cus => cus.Id == id));
+
+            return customerMocked.Object;
+        }
+
+        private List<Customer> Customers()
+        {
+            var fixture = new Fixture();
+            var customers = fixture.CreateMany<Customer>(50).ToList();
+            for (int i = 0; i < 50; i++)
+            {
+                customers[i].Id = i + 1;
+            }
+            return customers;
+        }
+    }
+}
